c5a6095150bc66afc79014f0963d748bb3b0c053
toguis <carlosmoranteg@yahoo.com>
2014-08-24 17:10:27 -0500

    First commit

---
+++ ToguisWebServices/IToguisSecurity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using ToguisModel;
+
+namespace ToguisWebServices
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IToguisSecurity" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IToguisSecurity
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "getuser?login={login}", ResponseFormat = WebMessageFormat.Json)]
+        TG_USER getUser(String login);
+     
+    }
+}
---
+++ ToguisWebServices/ToguisSecurity.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using ToguisModel;
+
+namespace ToguisWebServices
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ToguisSecurity" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ToguisSecurity.svc o ToguisSecurity.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ToguisSecurity : IToguisSecurity
+    {
+
+        public TG_USER getUser(string login)
+        {
+            TG_USER loResult = null; 
+            using (ToguisEntities loContext = new ToguisEntities())
+            {
+                try
+                {
+                    loContext.Configuration.ProxyCreationEnabled = false;
+                    loResult = (from item in loContext.TG_USER
+                                where item.USR_ID.ToLower().Equals(login)
+                                select item).FirstOrDefault();
+                }
+                catch (Exception ex)
+                {                    
+                }
+
+            }
+            return loResult;
+        }
+    }
+}
